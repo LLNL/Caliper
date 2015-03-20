ad80a60ca7b8ab984aab00629a10bec59b59bd28
David Boehme <boehme3@llnl.gov>
2015-03-20 16:07:54 -0700

    Make SigsafeRWLock work when other threads run concurrently with active signal handler

--- src/caliper/SigsafeRWLock.cpp
+++ src/caliper/SigsafeRWLock.cpp
@@ -5,9 +5,10 @@
 
 using namespace cali;
 
+volatile sig_atomic_t SigsafeRWLock::s_global_signal_flag { 0 };
 
 SigsafeRWLock::SigsafeRWLock()
-    : m_sigwlock { 0 }, m_sigrlock { 0 }
+    : m_sigwlock { 0 }, m_sigrlock { 0 }, m_sig_wflag { 0 }, m_sig_rflag { 0 }
 {
     pthread_rwlock_init(&m_rwlock, NULL);
 }
@@ -21,22 +22,42 @@ void SigsafeRWLock::rlock()
 {
     pthread_rwlock_rdlock(&m_rwlock);
     ++m_sigrlock;
+
+    // Spin while signal handler wlocked this mutex
+    while ( m_sig_wflag != 0 )
+        ;
 }
 
 void SigsafeRWLock::wlock()
 {
     pthread_rwlock_wrlock(&m_rwlock);
     ++m_sigwlock;
+
+    // Spin while signal handler rlocked or wlocked this mutex
+    while ( m_sig_rflag != 0 )
+        ;
+    while ( m_sig_wflag != 0 )
+        ;
 }
 
-bool SigsafeRWLock::sig_try_read()
+bool SigsafeRWLock::sig_try_rlock()
 {
-    return m_sigwlock == 0;        
+    if (m_sigwlock != 0)
+        return false;
+
+    m_sig_rflag = 1;
+
+    return true;
 }
 
-bool SigsafeRWLock::sig_try_write()
+bool SigsafeRWLock::sig_try_wlock()
 {
-    return m_sigwlock == 0 && m_sigrlock == 0;        
+    if (m_sigwlock != 0 || m_sigrlock != 0)
+        return false;
+
+    m_sig_wflag = 1;
+
+    return true;
 }
 
 void SigsafeRWLock::unlock()
@@ -47,4 +68,10 @@ void SigsafeRWLock::unlock()
         --m_sigwlock;
 
     pthread_rwlock_unlock(&m_rwlock);
+}
+
+void SigsafeRWLock::sig_unlock()
+{
+    m_sig_wflag = 0;
+    m_sig_rflag = 0;
 }
--- src/caliper/SigsafeRWLock.h
+++ src/caliper/SigsafeRWLock.h
@@ -12,9 +12,15 @@ namespace cali
 
 class SigsafeRWLock
 {
+    static volatile sig_atomic_t s_global_signal_flag;
+
     pthread_rwlock_t      m_rwlock;
+
     volatile sig_atomic_t m_sigwlock;
     volatile sig_atomic_t m_sigrlock;
+
+    volatile sig_atomic_t m_sig_wflag;
+    volatile sig_atomic_t m_sig_rflag;
 
 public:
 
@@ -25,10 +31,22 @@ public:
     void rlock();
     void wlock();
 
-    bool sig_try_read();
-    bool sig_try_write();
+    bool sig_try_rlock();
+    bool sig_try_wlock();
 
     void unlock();
+    void sig_unlock();
+
+    static void enter_signal() {
+        s_global_signal_flag = 1;
+    }
+    static void leave_signal() {
+        s_global_signal_flag = 0;
+    }
+
+    static bool is_in_signal() { 
+        return (s_global_signal_flag != 0);
+    }
 };
 
 }
