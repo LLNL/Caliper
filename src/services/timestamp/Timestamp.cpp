b4839be8cbe6a86ee7b7e7dd0b09736b37c8c9d4
David Boehme <boehme3@llnl.gov>
2015-03-25 18:55:28 -0700

    Extend timestamp service

--- src/services/timestamp/Timestamp.cpp
+++ src/services/timestamp/Timestamp.cpp
@@ -5,6 +5,7 @@
 
 #include <Caliper.h>
 
+#include <RuntimeConfig.h>
 #include <ContextRecord.h>
 #include <Log.h>
 
@@ -18,19 +19,66 @@ using namespace std;
 namespace 
 {
 
-Attribute attribute { Attribute::invalid } ;
 chrono::time_point<chrono::high_resolution_clock> tstart;
 
+Attribute timestamp_attr   { Attribute::invalid } ;
+Attribute timeoffs_attr    { Attribute::invalid } ;
+Attribute duration_attr    { Attribute::invalid } ;
+
+ConfigSet config;
+
+bool      record_timestamp;
+bool      record_offset;
+bool      record_duration;
+
+static const ConfigSet::Entry s_configdata[] = {
+    { "duration", CALI_TYPE_BOOL, "true",
+      "Include duration of context epoch with each context record",
+      "Include duration of context epoch with each context record"
+    },
+    { "offset", CALI_TYPE_BOOL, "false",
+      "Include time offset (time since program start) with each context record",
+      "Include time offset (time since program start) with each context record"
+    },
+    { "timestamp", CALI_TYPE_BOOL, "false",
+      "Include absolute timestamp (POSIX time) with each context record",
+      "Include absolute timestamp (POSIX time) with each context record"
+    },
+    ConfigSet::Terminator
+};
+
 void push_time(Caliper* c, int scope, WriteRecordFn fn) {
-    if (scope & CALI_SCOPE_PROCESS) {
-        auto now = chrono::high_resolution_clock::now();
-        uint64_t usec = chrono::duration_cast<chrono::microseconds>(now - tstart).count();
+    auto now = chrono::high_resolution_clock::now();
 
-        Variant v_attr(attribute.id());
-        Variant v_time(usec);
+    Variant v_attr[2];
+    Variant v_data[2];
 
-        int               n[3] = { 0, 1, 1 };
-        const Variant* data[3] = { nullptr, &v_attr, &v_time };
+    int count = 0;
+
+    if ((record_duration || record_offset) && scope & CALI_SCOPE_THREAD) {
+        uint64_t  usec = chrono::duration_cast<chrono::microseconds>(now - tstart).count();
+        Variant v_offs = c->exchange(timeoffs_attr, Variant(usec));
+
+        if (!v_offs.empty()) {
+            uint64_t duration = usec - v_offs.to_uint();
+
+            v_attr[count] = Variant(duration_attr.id());
+            v_data[count] = Variant(duration);
+
+            ++count;
+        }
+    }
+
+    if (record_timestamp && (scope & CALI_SCOPE_PROCESS)) {
+        v_attr[count] = Variant(timestamp_attr.id());
+        v_data[count] = Variant(static_cast<int>(chrono::system_clock::to_time_t(chrono::system_clock::now())));
+
+        ++count;
+    }
+
+    if (count) {
+        int               n[3] = { 0,       count,  count  };
+        const Variant* data[3] = { nullptr, v_attr, v_data };
 
         fn(ContextRecord::record_descriptor(), n, data);
     }
@@ -40,10 +88,27 @@ void push_time(Caliper* c, int scope, WriteRecordFn fn) {
 void timestamp_register(Caliper* c)
 {
     // set start time and create time attribute
-    tstart    = chrono::high_resolution_clock::now();
-    attribute = 
-        c->create_attribute("timestamp(usec)", CALI_TYPE_UINT, 
+    tstart = chrono::high_resolution_clock::now();
+
+    config = RuntimeConfig::init("timer", s_configdata);
+
+    record_duration  = config.get("duration").to_bool();
+    record_offset    = config.get("offset").to_bool();
+    record_timestamp = config.get("timestamp").to_bool();
+
+    int hide_offset  = (record_duration && !record_offset ? CALI_ATTR_HIDDEN : 0);
+
+    timestamp_attr = 
+        c->create_attribute("time.timestamp", CALI_TYPE_UINT, 
                             CALI_ATTR_ASVALUE | CALI_ATTR_SCOPE_PROCESS | CALI_ATTR_SKIP_EVENTS);
+    timeoffs_attr = 
+        c->create_attribute("time.end.offset", CALI_TYPE_UINT, 
+                            CALI_ATTR_ASVALUE | CALI_ATTR_SCOPE_THREAD  | CALI_ATTR_SKIP_EVENTS | hide_offset);
+    duration_attr = 
+        c->create_attribute("time.duration", CALI_TYPE_UINT, 
+                            CALI_ATTR_ASVALUE | CALI_ATTR_SCOPE_THREAD  | CALI_ATTR_SKIP_EVENTS);
+
+    c->set(timeoffs_attr, Variant(static_cast<uint64_t>(0)));
 
     // add callback for Caliper::get_context() event
     c->events().measure.connect(&push_time);
