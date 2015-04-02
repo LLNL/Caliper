5716b3d37671331f45a62b3a910fcc5b44cb62d0
David Boehme <boehme3@llnl.gov>
2015-04-02 12:55:51 -0700

    Minor beautification

--- cmake/FindLibunwind.cmake
+++ cmake/FindLibunwind.cmake
@@ -13,6 +13,6 @@ find_path(LIBUNWIND_INCLUDE_DIR libunwind.h)
 find_library(LIBUNWIND_LIBRARY unwind)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LIBUNWIND DEFAULT_MSG LIBUNWIND_LIBRARY LIBUNWIND_INCLUDE_DIR)
+find_package_handle_standard_args(Libunwind DEFAULT_MSG LIBUNWIND_LIBRARY LIBUNWIND_INCLUDE_DIR)
 
 mark_as_advanced(LIBUNWIND_LIBRARY LIBUNWIND_INCLUDE_DIR)
