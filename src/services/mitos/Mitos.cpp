e519fe6d29359caa5a5e78737ca142d23ba563dc
Alfredo Gimenez <gimenez1@llnl.gov>
2015-04-15 17:57:36 -0700

    added mitos attributes

--- src/services/mitos/Mitos.cpp
+++ src/services/mitos/Mitos.cpp
@@ -30,6 +30,11 @@ namespace
 {
 
 Attribute address_attr        { Attribute::invalid } ;
+Attribute latency_attr        { Attribute::invalid } ;
+Attribute timestamp_attr      { Attribute::invalid } ;
+Attribute ip_attr             { Attribute::invalid } ;
+Attribute datasource_attr     { Attribute::invalid } ;
+Attribute cpu_attr            { Attribute::invalid } ;
 
 ConfigSet config;
 
@@ -42,8 +47,8 @@ SigsafeRWLock thread_context_lock;
 
 static const ConfigSet::Entry s_configdata[] = {
     { "mitos", CALI_TYPE_BOOL, "false",
-      "Include sampled load addresses",
-      "Include sampled load addresses"
+      "Include memory access samples",
+      "Include memory access samples"
     },
     ConfigSet::Terminator
 };
@@ -75,13 +80,23 @@ void push_sample(Caliper* c, int scope, WriteRecordFn fn) {
 
     ContextBuffer *thread_context = find_context->second;
 
-    Variant v_attr[1];
-    Variant v_data[1];
+    Variant v_attr[6];
+    Variant v_data[6];
 
     v_attr[0] = address_attr.id();
     v_data[0] = static_sample.addr;
+    v_attr[1] = latency_attr.id();
+    v_data[1] = static_sample.weight;
+    v_attr[2] = timestamp_attr.id();
+    v_data[2] = static_sample.time;
+    v_attr[3] = ip_attr.id();
+    v_data[3] = static_sample.ip;
+    v_attr[4] = datasource_attr.id();
+    v_data[4] = static_sample.data_src;
+    v_attr[5] = cpu_attr.id();
+    v_data[5] = static_sample.cpu;
 
-    int               n[3] = { 0,       1,  1  };
+    int               n[3] = {       0,      6,      6 };
     const Variant* data[3] = { nullptr, v_attr, v_data };
 
     fn(ContextRecord::record_descriptor(), n, data);
@@ -116,9 +131,30 @@ void map_thread_context(cali_context_scope_t cscope,
 void mitos_register(Caliper* c) {
     record_address = config.get("address").to_bool();
 
-    address_attr = 
-        c->create_attribute("mitos.address", CALI_TYPE_ADDR, 
-                            CALI_ATTR_ASVALUE | CALI_ATTR_SCOPE_THREAD | CALI_ATTR_SKIP_EVENTS);
+    address_attr = c->create_attribute("mitos.address", CALI_TYPE_ADDR, 
+                                       CALI_ATTR_ASVALUE 
+                                       | CALI_ATTR_SCOPE_THREAD 
+                                       | CALI_ATTR_SKIP_EVENTS);
+    latency_attr = c->create_attribute("mitos.latency", CALI_TYPE_ADDR, 
+                                       CALI_ATTR_ASVALUE 
+                                       | CALI_ATTR_SCOPE_THREAD 
+                                       | CALI_ATTR_SKIP_EVENTS);
+    timestamp_attr = c->create_attribute("mitos.timestamp", CALI_TYPE_ADDR, 
+                                         CALI_ATTR_ASVALUE 
+                                         | CALI_ATTR_SCOPE_THREAD 
+                                         | CALI_ATTR_SKIP_EVENTS);
+    ip_attr = c->create_attribute("mitos.ip", CALI_TYPE_ADDR, 
+                                  CALI_ATTR_ASVALUE 
+                                  | CALI_ATTR_SCOPE_THREAD 
+                                  | CALI_ATTR_SKIP_EVENTS);
+    datasource_attr = c->create_attribute("mitos.datasource", CALI_TYPE_ADDR, 
+                                  CALI_ATTR_ASVALUE 
+                                  | CALI_ATTR_SCOPE_THREAD 
+                                  | CALI_ATTR_SKIP_EVENTS);
+    cpu_attr = c->create_attribute("mitos.cpu", CALI_TYPE_ADDR, 
+                                  CALI_ATTR_ASVALUE 
+                                  | CALI_ATTR_SCOPE_THREAD 
+                                  | CALI_ATTR_SKIP_EVENTS);
 
     // add callback for Caliper::get_context() event
     c->events().post_init_evt.connect(&mitos_init);
