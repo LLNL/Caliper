1e75a92157d2368a0dbf49713c8af7cbd10c67c4
David Boehme <boehme3@llnl.gov>
2015-06-26 10:42:31 -0700

    First step in service linking refactoring: Build libcaliper.a with all services linked in. Discards services with external dependencies for now (callpath, papi, mitos).

--- src/services/services.inc.cpp
+++ src/services/services.inc.cpp
@@ -5,19 +5,19 @@
 namespace cali
 {
 
-#ifdef CALIPER_HAVE_LIBUNWIND
-extern const CaliperService CallpathService;
-#endif
-#ifdef CALIPER_HAVE_PAPI
-extern const CaliperService PapiService;
-#endif
+// #ifdef CALIPER_HAVE_LIBUNWIND
+// extern const CaliperService CallpathService;
+// #endif
+// #ifdef CALIPER_HAVE_PAPI
+// extern const CaliperService PapiService;
+// #endif
 extern const CaliperService DebugService;
 extern const CaliperService PthreadService;
 extern const CaliperService RecorderService;
 extern const CaliperService TimestampService;
-#ifdef CALIPER_HAVE_MITOS
-extern const CaliperService MitosService;
-#endif
+// #ifdef CALIPER_HAVE_MITOS
+// extern const CaliperService MitosService;
+// #endif
 #ifdef CALIPER_HAVE_MPI
 extern const CaliperService MpiService;
 #endif
@@ -26,19 +26,19 @@ extern const CaliperService OmptService;
 #endif
 
 const CaliperService caliper_services[] = {
-#ifdef CALIPER_HAVE_LIBUNWIND
-    CallpathService,
-#endif
-#ifdef CALIPER_HAVE_PAPI
-    PapiService,
-#endif
+// #ifdef CALIPER_HAVE_LIBUNWIND
+//     CallpathService,
+// #endif
+// #ifdef CALIPER_HAVE_PAPI
+//     PapiService,
+// #endif
     DebugService,
     PthreadService,
     RecorderService,
     TimestampService,
-#ifdef CALIPER_HAVE_MITOS
-    MitosService,
-#endif
+// #ifdef CALIPER_HAVE_MITOS
+//     MitosService,
+// #endif
 #ifdef CALIPER_HAVE_MPI
     MpiService,
 #endif
