abbff70cbc07ffce89bfc50008320136dc240f69
Alfredo Gimenez <gimenez1@llnl.gov>
2016-02-23 15:32:02 -0800

    Python library for reading caliper files, calireader

---
+++ src/python/calireader.py
@@ -0,0 +1,7 @@
+from ctypes import cdll, py_object
+lib = cdll.LoadLibrary('./libcalireader.so')
+
+lib.read_cali_file.restype = py_object
+
+def read_cali_file(filename):
+    return lib.read_cali_file(filename)
