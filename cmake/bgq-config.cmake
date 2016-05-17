20383deb7ea01334395b07411bbbc650eb4bdea9
David Boehme <boehme3@llnl.gov>
2016-05-17 11:39:40 -0700

    BG/Q build config updates

--- cmake/bgq-config.cmake
+++ cmake/bgq-config.cmake
@@ -1,6 +1,11 @@
 set(CMAKE_C_COMPILER   "/usr/local/bin/bgclang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/usr/local/bin/bgclang++11" CACHE PATH "")
 
+# wtf ... not defined in vulcan's cmake platform config
+set(CMAKE_AR "/usr/bin/ar" CACHE PATH "")
+
+set(CMAKE_SYSTEM_NAME  "BlueGeneQ-static" CACHE STRING "")
+
 set(BUILD_SHARED_LIBS Off CACHE BOOL "")
 
 set(WITH_TOOLS Off CACHE BOOL "")
