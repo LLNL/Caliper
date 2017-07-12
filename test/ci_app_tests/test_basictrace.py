0b6aa1af4fcbd453cd2c8bfcc0a5c1f5a13599db
David Boehme <boehme3@llnl.gov>
2017-07-12 14:11:44 -0700

    tests: Add snapshot postprocess test case

--- test/ci_app_tests/test_basictrace.py
+++ test/ci_app_tests/test_basictrace.py
@@ -86,5 +86,24 @@ class CaliperBasicTraceTest(unittest.TestCase):
                 'cali.attribute.prop' : '12',
                 'cali.attribute.type' : 'string' }))
 
+    def test_postprocess_snapshot(self):
+        target_cmd = [ './ci_test_postprocess_snapshot' ]
+        query_cmd  = [ '../../src/tools/cali-query/cali-query', '-e' ]
+
+        caliper_config = {
+            'CALI_SERVICES_ENABLE'   : 'trace:recorder',
+            'CALI_RECORDER_FILENAME' : 'stdout',
+            'CALI_LOG_VERBOSITY'     : '0',
+        }
+
+        query_output = calitest.run_test_with_query(target_cmd, query_cmd, caliper_config)
+        snapshots = calitest.get_snapshots_from_text(query_output)
+
+        self.assertTrue(calitest.has_snapshot_with_attributes(
+            snapshots, {
+                'snapshot.val'     : '49',
+                'postprocess.val'  : '42',
+                'postprocess.node' : '36' }))
+
 if __name__ == "__main__":
     unittest.main()
