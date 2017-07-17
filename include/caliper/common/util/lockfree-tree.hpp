95e11a9a395879cd4cc0af310aaf0b737b687014
David Boehme <boehme3@llnl.gov>
2017-07-17 13:50:34 -0700

    Documentation updates

--- include/caliper/common/util/lockfree-tree.hpp
+++ include/caliper/common/util/lockfree-tree.hpp
@@ -9,8 +9,38 @@
 namespace util
 {
 
-/// \brief A thread-safe tree class that allows lockfree appending of nodes.
-
+/// \brief A thread-safe, lock-free, intrusive tree data structure.
+///
+/// Implements a tree data structure. Each tree node has a list of
+/// child nodes, and links to its parent node and its next sibling.
+///
+/// This is an \e intrusive data structure, i.e., the tree node
+/// elements are members of the host data structure. Hosts must add a
+/// \c LockfreeIntrusiveTree<Host>::Node element and point a tree
+/// object to it.  The host can be part of multiple independent trees.
+/// Example:
+///
+/// \code
+/// struct Host {
+///     LockfreeIntrusiveTree<Host>::Node tree_a;
+///     LockfreeIntrusiveTree<Host>::Node tree_b;
+///     // other host data structure members
+/// };
+///
+/// // use the tree:
+/// Host root, a_child, b_child;
+/// LockfreeIntrusiveTree<Host>(&root, &Host::tree_a).append(&a_child);
+/// LockfreeIntrusiveTree<Host>(&root, &Host::tree_b).append(&b_child);
+/// \endcode
+///
+/// The tree can be safely used from multiple threads without locking
+/// (however, note that this applies only to the tree structure
+/// itself; other data elements in the host structure are not
+/// protected).  Because of its lock-free nature, there are
+/// restrictions on the operations that can be
+/// performed. Specifically, tree nodes can only be added, but not
+/// moved or removed.
+   
 template<typename T> 
 class LockfreeIntrusiveTree {
 public:
@@ -40,7 +70,6 @@ private:
     LockfreeIntrusiveTree<T>::Node& node(T* t) const { return node(t, m_node); }
 
 public:
-
 
     LockfreeIntrusiveTree(T* me, LockfreeIntrusiveTree<T>::Node T::*nodeptr) 
         : m_me(me), m_node(nodeptr)
