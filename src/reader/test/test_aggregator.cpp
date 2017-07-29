f7007ee25ca9d0b9a337d6a60eed19493bdefe2f
David Boehme <boehme3@llnl.gov>
2017-07-29 09:51:18 -0700

    tests: Add aggregator test case for empty key

--- src/reader/test/test_aggregator.cpp
+++ src/reader/test/test_aggregator.cpp
@@ -309,6 +309,11 @@ TEST(AggregatorTest, SingleKeySumOpSpec) {
     spec.aggregation_ops.list.push_back(::make_op("count"));
     spec.aggregation_ops.list.push_back(::make_op("sum", "val"));
 
+    ASSERT_EQ(static_cast<int>(spec.aggregation_ops.list.size()), 2);
+    
+    ASSERT_STREQ(spec.aggregation_ops.list[0].op.name, "count"); // see if kernel lookup went OK
+    ASSERT_STREQ(spec.aggregation_ops.list[1].op.name, "sum");
+
     Aggregator a(spec);
     
     // add some entries
@@ -365,4 +370,100 @@ TEST(AggregatorTest, SingleKeySumOpSpec) {
         });
 
     EXPECT_EQ(rescount, 2);
+}
+
+TEST(AggregatorTest, NoneKeySumOpSpec) {
+    //
+    // --- setup
+    //
+    
+    CaliperMetadataDB db;
+    IdMap             idmap;
+
+    // create some context attributes
+    
+    Attribute ctx1 =
+        db.create_attribute("ctx.1", CALI_TYPE_STRING, CALI_ATTR_DEFAULT);
+    Attribute ctx2 = 
+        db.create_attribute("ctx.2", CALI_TYPE_INT,    CALI_ATTR_DEFAULT);
+    Attribute val_attr =
+        db.create_attribute("val",   CALI_TYPE_INT,    CALI_ATTR_ASVALUE);
+
+    // make some nodes
+    
+    const struct NodeInfo {
+        cali_id_t node_id;
+        cali_id_t attr_id;
+        cali_id_t prnt_id;
+        Variant   data;
+    } test_nodes[] = {
+        { 100, ctx1.id(), CALI_INV_ID, Variant(CALI_TYPE_STRING, "outer", 6) },
+        { 101, ctx2.id(), 100,         Variant(42)                           },
+        { 102, ctx1.id(), 101,         Variant(CALI_TYPE_STRING, "inner", 6) }
+    };
+
+    const Node* node = nullptr;
+
+    for ( const NodeInfo& nI : test_nodes )
+        node = db.merge_node(nI.node_id, nI.attr_id, nI.prnt_id, nI.data, idmap);
+
+    //
+    // --- Test with ctx2 key, count and sum kernel
+    //
+
+    QuerySpec spec;
+
+    spec.aggregation_key.selection = QuerySpec::SelectionList<std::string>::None;
+    
+    spec.aggregation_ops.selection = QuerySpec::SelectionList<QuerySpec::AggregationOp>::List;
+    spec.aggregation_ops.list.push_back(::make_op("count"));
+    spec.aggregation_ops.list.push_back(::make_op("sum", "val"));
+
+    ASSERT_EQ(static_cast<int>(spec.aggregation_ops.list.size()), 2);
+    
+    ASSERT_STREQ(spec.aggregation_ops.list[0].op.name, "count"); // see if kernel lookup went OK
+    ASSERT_STREQ(spec.aggregation_ops.list[1].op.name, "sum");
+
+    Aggregator a(spec);
+    
+    // add some entries
+
+    Variant   v_val(7);
+    
+    cali_id_t node_ctx1 = 102;
+    cali_id_t node_ctx2 = 101;
+    cali_id_t node_ctx3 = 100;
+    cali_id_t val_id    = val_attr.id();
+
+    a.add(db, db.merge_snapshot(1, &node_ctx1, 1, &val_id, &v_val,  idmap));
+    a.add(db, db.merge_snapshot(1, &node_ctx1, 0, nullptr, nullptr, idmap));
+    a.add(db, db.merge_snapshot(1, &node_ctx1, 1, &val_id, &v_val,  idmap));
+    a.add(db, db.merge_snapshot(1, &node_ctx2, 1, &val_id, &v_val,  idmap));
+    a.add(db, db.merge_snapshot(1, &node_ctx2, 0, nullptr, nullptr, idmap));
+    a.add(db, db.merge_snapshot(0, nullptr,    1, &val_id, &v_val,  idmap));
+    a.add(db, db.merge_snapshot(0, nullptr,    0, nullptr, nullptr, idmap));
+    a.add(db, db.merge_snapshot(1, &node_ctx3, 1, &val_id, &v_val,  idmap));
+    a.add(db, db.merge_snapshot(1, &node_ctx3, 0, nullptr, nullptr, idmap));
+        
+    std::vector<EntryList> resdb;
+
+    a.flush(db, [&resdb](CaliperMetadataAccessInterface&, const EntryList& list) {
+            resdb.push_back(list);
+        });
+
+    Attribute count_attr = db.get_attribute("aggregate.count");
+
+    ASSERT_NE(count_attr, Attribute::invalid);
+
+    // check results
+    
+    EXPECT_EQ(resdb.size(), 1); // one for (empty key)
+
+    auto dict  = make_dict_from_entrylist(resdb.front());
+                
+    int  count = dict[count_attr.id()].value().to_int();
+    int  val   = dict[val_attr.id()  ].value().to_int();
+
+    EXPECT_EQ(count,  9);
+    EXPECT_EQ(val,   35);
 }
