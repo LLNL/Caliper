da253794820b201a5e635aba6f1eda004cbcf453
David Boehme <boehme3@llnl.gov>
2017-11-06 10:42:14 -0800

    Add test for reader CSV output

--- test/ci_app_tests/test_report.py
+++ test/ci_app_tests/test_report.py
@@ -5,7 +5,29 @@ import unittest
 import calipertest as cat
 
 class CaliperReportTest(unittest.TestCase):
-    """ Caliper Report / C API test case """
+    """ Caliper Report / Reader CSV export / C config API test case """
+
+    def test_report_csvwriter(self):
+        """ Test reader lib's CSV export via report service """
+        
+        target_cmd = [ './ci_test_macros' ]
+        query_cmd  = [ '../../src/tools/cali-query/cali-query',
+                       '-q', 'SELECT count(),iteration#fooloop WHERE loop=fooloop GROUP BY iteration#fooloop FORMAT expand' ]
+
+        caliper_config = {
+            'CALI_SERVICES_ENABLE'   : 'event,trace,report',
+            'CALI_REPORT_CONFIG'     : 'format csv',
+            'CALI_LOG_VERBOSITY'     : '0'
+        }
+
+        query_output = cat.run_test_with_query(target_cmd, query_cmd, caliper_config)
+        snapshots = cat.get_snapshots_from_text(query_output)
+
+        self.assertTrue(len(snapshots) == 5)
+
+        self.assertTrue(cat.has_snapshot_with_attributes(
+            snapshots, { 'iteration#fooloop': '3', 'count': '1' }))
+
 
     def test_report(self):
         target_cmd = [ './ci_test_report' ]
