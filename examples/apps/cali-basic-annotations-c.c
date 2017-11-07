507cdbf7dbd4d3fe7d7df19e1722d472e2e26059
David Boehme <boehme3@llnl.gov>
2017-11-07 14:20:08 -0800

    Move example apps into examples/ dir

--- examples/apps/cali-basic-annotations-c.c
+++ examples/apps/cali-basic-annotations-c.c
@@ -1,6 +1,8 @@
 /*
  * cali-basic-c
- * Caliper C interface test
+ * Caliper C interface example
+ *
+ * Demonstrate the use of Caliper's annotation macros in a C program.
  */
 
 #include <caliper/cali.h>
@@ -10,17 +12,25 @@
 
 void foo(int count)
 {
+  /* Mark begin of a function. Sets "function=foo" in Caliper. */
   CALI_MARK_FUNCTION_BEGIN;
 
+  /* Export "cali-demo.foo.loopcount=<count>" via the low-level C annotation API. */
   cali_set_int_byname("cali-demo.foo.loopcount", count);
 
-  /* Mark foo loop and set loopcount information */
+  double *d = (double*) malloc(count*sizeof(double));
 
-  double *d = (double*)malloc(count*sizeof(double));
+  if (!d) {
+    /* All function exits must be marked! */
+    CALI_MARK_FUNCTION_END;
+    return;
+  }
 
+  /* Mark a loop. Sets "loop=cali-demo.fooloop" in Caliper. */ 
   CALI_MARK_LOOP_BEGIN(fooloop, "cali-demo.fooloop");
   
   for (int i = 0; i < count; ++i) {
+    /* Mark a loop iteration. Sets "iteration#cali-demo.fooloop=<i>" in Caliper. */
     CALI_MARK_ITERATION_BEGIN(fooloop, i);
 
     /* do work */
@@ -40,15 +50,10 @@ int main(int argc, char* argv[])
 {
   CALI_MARK_FUNCTION_BEGIN;
 
-  /* --- Mark initialization phase --- */ 
-
+  /* Marking a single C statement. Sets "statement=cali-demo.init" in Caliper. */ 
   CALI_WRAP_STATEMENT("cali-demo.init", int count = argc > 1 ? atoi(argv[1]) : 4);
 
-  /* --- Mark foo kernel --- */
-
   foo(count);
-
-  /* --- Finalization --- */
 
   CALI_MARK_FUNCTION_END;
 }
