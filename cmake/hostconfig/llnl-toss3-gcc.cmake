86ea5dc279ff861e4f5be5ad582412e3fd844842
David Boehme <boehme3@llnl.gov>
2018-06-19 10:35:16 -0700

    Update LLNL system hostconfigs

--- cmake/hostconfig/llnl-toss3-gcc.cmake
+++ cmake/hostconfig/llnl-toss3-gcc.cmake
@@ -4,15 +4,16 @@ set(CMAKE_CXX_COMPILER "/usr/tce/bin/g++" CACHE PATH "C++ Compiler (g++ 4.9.3)")
 set(MPI_C_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3/bin/mpicc" CACHE PATH "")
 set(MPI_CXX_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3/bin/mpicxx" CACHE PATH "")
 
-set(PAPI_PREFIX "/usr/tce/packages/papi/papi-5.4.3" CACHE PATH "")
+set(PAPI_PREFIX "/usr/tce/packages/papi/papi-5.5.1" CACHE PATH "")
 set(ITT_PREFIX  "/usr/tce/packages/vtune/default" CACHE PATH "")
 
-set(CMAKE_PREFIX_PATH "/usr/tce/packages/dyninst/dyninst-9.3.1/lib/cmake" CACHE PATH "")
+set(CMAKE_PREFIX_PATH "/usr/tce/packages/dyninst/dyninst-9.3.2/lib/cmake" CACHE PATH "")
 
 set(WITH_CALLPATH On  CACHE BOOL "")
 set(WITH_NVPROF   Off CACHE BOOL "")
 set(WITH_CUPTI    Off CACHE BOOL "")
 set(WITH_PAPI     On  CACHE BOOL "")
+set(WITH_LIBPFM   On  CACHE BOOL "")
 set(WITH_DYNINST  On  CACHE BOOL "")
 set(WITH_SAMPLER  On  Cache BOOL "")
 set(WITH_MPI      On  CACHE BOOL "")
