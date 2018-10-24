853c4de80004aa19ccfc22d3c9fb69a32d29c80d
David Poliakoff <david.poliakoff@gmail.com>
2018-10-24 10:59:24 -0700

    Initial implementation

--- include/caliper/CaliFunctional.h
+++ include/caliper/CaliFunctional.h
@@ -43,6 +43,8 @@
 
 #include <cstring>
 #include <iostream>
+#include <sstream>
+#include <vector>
 #include <tuple>
 #include <type_traits>
 
@@ -54,9 +56,10 @@ public:
         inner_annot = Annotation(name,opt);
     }
     SafeAnnotation(SafeAnnotation& other) : inner_annot(other.getAnnot()) {
-        
     }
-    Annotation& getAnnot() {
+    SafeAnnotation(const SafeAnnotation& other) : inner_annot(other.getAnnot()) {
+    }
+    const Annotation& getAnnot() const {
         return inner_annot;
     }
     SafeAnnotation& operator = (SafeAnnotation& other){
@@ -108,17 +111,32 @@ public:
         inner_annot.set(data);
         return *this;
     }
+    SafeAnnotation& set(unsigned long data){
+      // Bad, but seemingly cali-necessary
+        inner_annot.set((int)data);
+        return *this;
+    }
     void end(){
         inner_annot.end();
     }
     template<typename T>
-    SafeAnnotation & begin(T start){
-        inner_annot.begin("Unmeasurable");
+    SafeAnnotation & set(T* start){
+        // dumb
+        std::ostringstream address;
+        address << (void const *)start;
+        std::string name = address.str();
+        //auto foo = 
+        inner_annot.set(  Variant(CALI_TYPE_STRING, name.c_str(), strlen(name.c_str())) ) ;
         return *this;
     }
     template<typename T>
-    SafeAnnotation & set(T start){
-        inner_annot.set("Unmeasurable");
+    SafeAnnotation & set(const T* start){
+        // dumb
+        std::ostringstream address;
+        address << (void const *)start;
+        std::string name = address.str();
+        auto foo = Variant(CALI_TYPE_STRING, name.c_str(), strlen(name.c_str()));
+        inner_annot.set(foo);
         return *this;
     }
     Annotation inner_annot;
@@ -346,6 +364,106 @@ template<typename LB>
 RecordedFunction<LB> make_recorded_function(const char* name, LB body){
     return RecordedFunction<LB>(name,body);
 }
+
+namespace bluthund{
+using AnnotationType = cali::Annotation;
+using AnnotationVector = std::vector<AnnotationType>;
+
+template<typename... Args>
+struct Nase{
+    static auto record(int arg_num, AnnotationVector annotations) -> void {
+    }
+};
+template<typename T>
+struct recordValue;
+template<typename T>
+struct recordValue{
+static void record(AnnotationType& annot, T arg){
+annot.set(arg);
+}
+};
+template<>
+struct recordValue<unsigned long>{
+static void record(AnnotationType& annot, unsigned long in){
+annot.set((int)in);
+}
+};
+template<typename T>
+struct recordValue<T*>{
+static void record(AnnotationType& annot, T* in){
+  annot.set((int)(unsigned long)in);
+}
+};
+template<typename T>
+struct recordValue<const T*>{
+static void record(AnnotationType& annot, const T* in){
+  annot.set((int)(unsigned long)in);
+}
+};
+template<typename T>
+void recorder_helper(AnnotationType& annot, T in){
+  recordValue<T>::record(annot,in);
+}
+template <typename Arg, typename... Args>
+struct Nase<Arg, Args...>{
+    using NextRecorder = Nase<Args...>;
+    
+    static auto record(int arg_num, AnnotationVector annotations, Arg arg, Args... args) -> void {
+       //annotations[arg_num].set(arg);
+       recorder_helper(annotations[arg_num],arg);
+       NextRecorder::record(arg_num + 1, annotations, args...);
+    }
+};
+
+void* rip_the_rip(){
+  return __builtin_return_address(0);
+}
+
+template<class LB>
+struct TrackedExecutor {
+    TrackedExecutor(std::string func_name, LB func, std::vector<std::string> arg_names_in, std::string returned_name_in) : func_annot(func_name.c_str()), body(func), returned_annotation(returned_name_in.c_str(), CALI_ATTR_SKIP_EVENTS), rip_annot("rip", CALI_ATTR_SKIP_EVENTS){
+        for(auto name : arg_names_in){
+          argument_annotations.push_back(AnnotationType(name.c_str(), CALI_ATTR_SKIP_EVENTS));
+        }
+    }
+    template <typename... Args>
+    auto untracked_call(Args... args) -> typename std::result_of<LB(Args...)>::type 
+    {
+      return body(args...);
+    }
+    template <typename... Args>
+    auto operator()(Args... args) -> typename std::enable_if<
+        !std::is_same<typename std::result_of<LB(Args...)>::type, void>::value,
+        typename std::result_of<LB(Args...)>::type>::type {
+      cali::Annotation::Guard func_annot_guard(func_annot.begin());
+      Nase<Args...>::record(0,argument_annotations,args...);
+      recorder_helper(rip_annot,rip_the_rip());
+      auto return_value = body(args...);
+      recorder_helper(returned_annotation, return_value);
+      return return_value;
+    }
+    template <typename... Args>
+    auto operator()(Args... args) -> typename std::enable_if<
+        std::is_same<typename std::result_of<LB(Args...)>::type, void>::value,
+        typename std::result_of<LB(Args...)>::type>::type {
+      cali::Annotation::Guard func_annot_guard(func_annot);
+      Nase<Args...>::record(0,argument_annotations,args...);
+      recorder_helper(rip_annot,rip_the_rip());
+      return body(args...);
+    }
+    LB body;
+    AnnotationType func_annot;
+    AnnotationVector argument_annotations;
+    AnnotationType returned_annotation;
+    AnnotationType rip_annot;
+};
+
+template<typename Callable>
+TrackedExecutor<Callable> wrap(std::string name, Callable callee, std::vector<std::string> arg_names, std::string returned_name){
+  return TrackedExecutor<Callable>(name, callee, arg_names, returned_name);
+}
+
+} // end namespace bluthund
 
 } // end namespace cali
 #endif //CALI_ANNOTATED_REGION_H
