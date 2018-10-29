5398118c7a509190525730f71328b5d374ee17db
David Boehme <boehme3@llnl.gov>
2018-10-29 13:08:07 -0700

    Use LIBUNWIND_PREFIX to find libunwind

--- cmake/FindLibunwind.cmake
+++ cmake/FindLibunwind.cmake
@@ -2,17 +2,21 @@
 # - Find libunwind
 #
 # LIBUNWIND_INCLUDE_DIR - Path to libunwind.h
-# LIBUNWIND_LIBRARY     - List of libraries for using libunwind
+# LIBUNWIND_LIBRARIES   - List of libraries for using libunwind
 # LIBUNWIND_FOUND       - True if libunwind was found
 
-if(LIBUNWIND_INCLUDE_DIR)
-  set(LIBUNWIND_FIND_QUIETLY true)
-endif()
+find_path(LIBUNWIND_PREFIX
+  include/libunwind.h)
 
-find_path(LIBUNWIND_INCLUDE_DIR libunwind.h)
-find_library(LIBUNWIND_LIBRARY unwind)
+find_library(LIBUNWIND_LIBRARIES
+  NAMES unwind
+  HINTS ${LIBUNWIND_PREFIX}/lib)
+
+find_path(LIBUNWIND_INCLUDE_DIR
+  NAMES libunwind.h
+  HINTS ${LIBUNWIND_PREFIX}/include)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Libunwind DEFAULT_MSG LIBUNWIND_LIBRARY LIBUNWIND_INCLUDE_DIR)
+find_package_handle_standard_args(Libunwind DEFAULT_MSG LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIR)
 
-mark_as_advanced(LIBUNWIND_LIBRARY LIBUNWIND_INCLUDE_DIR)
+mark_as_advanced(LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIR)
