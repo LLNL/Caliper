bd7d2258c9c34e3787cb5cd424eab2c195d7afc5
David Boehme <boehme3@llnl.gov>
2018-10-29 13:19:24 -0700

    Update comments and use _DIRS consistently in FindLibunwind.cmake

--- cmake/FindLibunwind.cmake
+++ cmake/FindLibunwind.cmake
@@ -1,6 +1,8 @@
 #
 # - Find libunwind
 #
+# LIBUNWIND_PREFIX      - Set to the libunwind installation directory
+#
 # LIBUNWIND_INCLUDE_DIR - Path to libunwind.h
 # LIBUNWIND_LIBRARIES   - List of libraries for using libunwind
 # LIBUNWIND_FOUND       - True if libunwind was found
@@ -12,11 +14,11 @@ find_library(LIBUNWIND_LIBRARIES
   NAMES unwind
   HINTS ${LIBUNWIND_PREFIX}/lib)
 
-find_path(LIBUNWIND_INCLUDE_DIR
+find_path(LIBUNWIND_INCLUDE_DIRS
   NAMES libunwind.h
   HINTS ${LIBUNWIND_PREFIX}/include)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Libunwind DEFAULT_MSG LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIR)
+find_package_handle_standard_args(Libunwind DEFAULT_MSG LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIRS)
 
-mark_as_advanced(LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIR)
+mark_as_advanced(LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIRS)
