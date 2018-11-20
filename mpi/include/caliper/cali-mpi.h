18366448c85c8aebc15976a55f4260648fe9ef66
David Boehme <boehme3@llnl.gov>
2018-11-20 12:30:33 -0800

    mpi: Add cali_mpi_init() to make sure libcaliper-mpi is linked

--- mpi/include/caliper/cali-mpi.h
+++ mpi/include/caliper/cali-mpi.h
@@ -84,6 +84,22 @@ extern "C" {
  * C definitions
  */
 
+/**
+ * \brief Initialize caliper-mpi library. 
+ *   Must run before Caliper initialization.
+ *
+ * Normally, the Caliper mpi library is initialized through a dynamic 
+ * library constructor in libcaliper-mpi.so, and it is not necessary to call
+ * cali_mpi_init(). However, certain linker optimizations or static linking 
+ * can prevent a library from being linked if a library constructor is 
+ * the only entry point. In this case, call cali_mpi_init() from the target 
+ * application before initializing Caliper. It ensures that the Caliper mpi 
+ * runtime library (libcaliper-mpi) is correctly linked to the application, 
+ * and makes the mpi-specific services visible to the Caliper initializer.
+ */
+    
+void
+cali_mpi_init();
     
 #ifdef __cplusplus
 } /* extern "C" */
