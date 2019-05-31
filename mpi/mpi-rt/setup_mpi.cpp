20c2bee7aba40e6d4f8196220c45c5ae035e1284
David Boehme <boehme3@llnl.gov>
2019-05-31 14:52:46 -0700

    Add Spot v1 controller

--- mpi/mpi-rt/setup_mpi.cpp
+++ mpi/mpi-rt/setup_mpi.cpp
@@ -65,9 +65,11 @@ CaliperService cali_mpi_services[] = {
 };
 
 extern ConfigManager::ConfigInfo spot_controller_info;
+extern ConfigManager::ConfigInfo spot_v1_controller_info;
 
 ConfigManager::ConfigInfo mpi_controllers[] = {
     spot_controller_info,
+    spot_v1_controller_info,
 
     { nullptr, nullptr, nullptr }
 };
