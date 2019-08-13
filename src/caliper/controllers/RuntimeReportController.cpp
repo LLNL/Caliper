a5071e24fa88c2d80bdb93cef683ba7acebd68bf
David Boehme <boehme3@llnl.gov>
2019-08-12 17:41:23 -0700

    Add mem.highwatermark profiling option

--- src/caliper/controllers/RuntimeReportController.cpp
+++ src/caliper/controllers/RuntimeReportController.cpp
@@ -19,7 +19,8 @@ namespace
 enum ProfileConfig {
     WrapMpi    = 1,
     WrapCuda   = 2,
-    MallocInfo = 4
+    MallocInfo = 4,
+    MemHighWaterMark = 8
 };
 
 class RuntimeReportController : public cali::ChannelController
@@ -36,52 +37,57 @@ public:
                 { "CALI_TIMER_UNIT", "sec" }
             })
         {
+            std::string select;
+            std::string groupby = "prop:nested";
+
             if (use_mpi) {
-                std::string query =
-                    " group by prop:nested format tree"
-                    " select min(sum#time.duration) as \"Min time/rank\""
+                select =
+                    " min(sum#time.duration) as \"Min time/rank\""
                     ",max(sum#time.duration) as \"Max time/rank\""
                     ",avg(sum#time.duration) as \"Avg time/rank\""
                     ",percent_total(sum#time.duration) as \"Time % (total)\"";
-
-                if (profile & MallocInfo)
-                    query +=
-                        ",sum(sum#malloc.bytes) as \"Heap Allocations\""
-                        ",max(max#malloc.total.bytes) as \"Max Total Bytes on Heap\"";
-
-                config()["CALI_SERVICES_ENABLE"   ].append(",mpireport");
-                config()["CALI_MPIREPORT_FILENAME"] = output;
-                config()["CALI_MPIREPORT_WRITE_ON_FINALIZE"] = "false";
-                config()["CALI_MPIREPORT_CONFIG"  ] = query;
             } else {
-                std::string query =
-                    " group by prop:nested format tree"
-                    " select inclusive_sum(sum#time.duration) as \"Inclusive time\""
+                select =
+                    " inclusive_sum(sum#time.duration) as \"Inclusive time\""
                     ",sum(sum#time.duration) as \"Exclusive time\""
                     ",percent_total(sum#time.duration) as \"Time %\"";
-
-                if (profile & MallocInfo)
-                    query +=
-                        ",sum(sum#malloc.bytes) as \"Heap Allocations\""
-                        ",max(max#malloc.total.bytes) as \"Max Total Bytes on Heap\"";
-                    
-                config()["CALI_SERVICES_ENABLE"   ].append(",report");
-                config()["CALI_REPORT_FILENAME"   ] = output;
-                config()["CALI_REPORT_CONFIG"     ] = query;
             }
 
             if (profile & MallocInfo) {
-                config()["CALI_SERVICES_ENABLE"   ].append(",memusage");
+                select +=
+                    ",sum(sum#malloc.bytes) as \"Heap Allocations\""
+                    ",max(max#malloc.total.bytes) as \"Max Total Bytes on Heap\"";
+
+                config()["CALI_SERVICES_ENABLE"   ].append(",mallinfo");
             }
-            
+            if (profile & MemHighWaterMark) {
+                select += 
+                    ",max(max#alloc.region.highwatermark) as \"Max Alloc'd Mem\"";
+                
+                config()["CALI_SERVICES_ENABLE"   ].append(",alloc,sysalloc");
+                config()["CALI_ALLOC_RECORD_HIGHWATERMARK"] = "true";
+                config()["CALI_ALLOC_TRACK_ALLOCATIONS"   ] = "false";
+            }
             if (profile & WrapMpi) {
                 config()["CALI_SERVICES_ENABLE"   ].append(",mpi");
                 config()["CALI_MPI_BLACKLIST"     ] =
                     "MPI_Comm_rank,MPI_Comm_size,MPI_Wtick,MPI_Wtime";
             }
-
             if (profile & WrapCuda) {
                 config()["CALI_SERVICES_ENABLE"   ].append(",cupti");
+            }
+
+            if (use_mpi) {
+                config()["CALI_SERVICES_ENABLE"   ].append(",mpireport");
+                config()["CALI_MPIREPORT_FILENAME"] = output;
+                config()["CALI_MPIREPORT_WRITE_ON_FINALIZE"] = "false";
+                config()["CALI_MPIREPORT_CONFIG"  ] = 
+                    std::string("select ") + select + " group by " + groupby + " format tree";
+            } else {
+                config()["CALI_SERVICES_ENABLE"   ].append(",report");
+                config()["CALI_REPORT_FILENAME"   ] = output;
+                config()["CALI_REPORT_CONFIG"     ] = 
+                    std::string("select ") + select + " group by " + groupby + " format tree";
             }
         }
 };
@@ -126,17 +132,18 @@ get_profile_cfg(const cali::ConfigManager::argmap_t& args)
     Services::add_default_services();
     auto srvcs = Services::get_available_services();
 
-    const std::vector< std::tuple<std::string, ProfileConfig, std::string> > profinfo {
+    const std::vector< std::tuple<const char*, ProfileConfig, const char*> > profinfo {
         { std::make_tuple( "mpi",    WrapMpi,    "mpi")      },
         { std::make_tuple( "cuda",   WrapCuda,   "cupti")    },
-        { std::make_tuple( "malloc", MallocInfo, "memusage") }
+        { std::make_tuple( "malloc", MallocInfo, "memusage") },
+        { std::make_tuple( "mem.highwatermark", MemHighWaterMark, "sysalloc" )}
     };
 
     int profile = 0;
     
     for (const std::string& s : StringConverter(argit->second).to_stringlist(",:")) {
         auto it = std::find_if(profinfo.begin(), profinfo.end(), [s](decltype(profinfo.front()) tpl){
-                return std::get<0>(tpl) == s;
+                return s == std::get<0>(tpl);
             });
 
         if (it == profinfo.end())
