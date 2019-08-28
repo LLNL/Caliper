c2bca91f61538886c34ecdf58ea4448126e97678
David Boehme <boehme3@llnl.gov>
2019-08-27 17:05:24 -0700

    Make event-trace controller configurable

--- src/caliper/controllers/EventTraceController.cpp
+++ src/caliper/controllers/EventTraceController.cpp
@@ -1,48 +1,93 @@
 #include "caliper/ChannelController.h"
 #include "caliper/ConfigManager.h"
 
+#include "caliper/common/StringConverter.h"
+
 using namespace cali;
 
 namespace
 {
 
+enum TraceOpts {
+    Timestamps = 1,
+    TraceMpi   = 2,
+    TraceCuda  = 4,
+    TraceIo    = 8
+};
+
 class EventTraceController : public cali::ChannelController
 {
 public:
-    
-    EventTraceController(const std::string& output)
+
+    EventTraceController(const std::string& output, int opts)
         : ChannelController("event-trace", 0, {
                 { "CALI_CHANNEL_FLUSH_ON_EXIT", "false" },
-                { "CALI_CONFIG_PROFILE", "serial-trace" }
+                { "CALI_SERVICES_ENABLE", "event,recorder,trace,timestamp" },
+                { "CALI_TIMER_UNIT", "sec" },
+                { "CALI_TIMER_SNAPSHOT_DURATION", "true" }
             })
         {
             std::string tmp = output;
-            
+
             if (!tmp.empty()) {
                 if (tmp != "stderr" && tmp != "stdout") {
                     auto pos = tmp.find_last_of('.');
-                
+
                     if (pos == std::string::npos || tmp.substr(pos) != ".cali")
                         tmp.append(".cali");
                 }
-                
+
                 config()["CALI_RECORDER_FILENAME"] = tmp;
+            }
+
+            if (opts & Timestamps) {
+                config()["CALI_TIMER_OFFSET"   ] = "true";
+            }
+            if (opts & TraceMpi) {
+                config()["CALI_SERVICES_ENABLE"].append(",mpi");
+                config()["CALI_MPI_BLACKLIST"  ] =
+                    "MPI_Comm_size,MPI_Comm_rank,MPI_Wtime";
+            }
+            if (opts & TraceCuda) {
+                config()["CALI_SERVICES_ENABLE"].append(",cupti");
+            }
+            if (opts & TraceIo) {
+                config()["CALI_SERVICES_ENABLE"].append(",io");
             }
         }
 };
 
-const char* event_trace_args[] = { "output", nullptr };
+const char* event_trace_args[] = {
+    "output", "event.timestamps", "trace.io", "trace.mpi", "trace.cuda", nullptr
+};
 
 static cali::ChannelController*
 make_event_trace_controller(const cali::ConfigManager::argmap_t& args)
 {
     std::string output;
     auto it = args.find("output");
-    
+
     if (it != args.end())
         output = it->second;
-        
-    return new EventTraceController(output);
+
+    int traceopts = 0;
+
+    const struct {
+        const char* optname; int opt;
+    } trace_info[] = {
+        { "event.timestamps", Timestamps },
+        { "trace.mpi",        TraceMpi   },
+        { "trace.cuda",       TraceCuda  },
+        { "trace.io",         TraceIo    }
+    };
+
+    for (auto &info : trace_info) {
+        auto it = args.find(info.optname);
+        if (it != args.end() && StringConverter(it->second).to_bool())
+            traceopts |= info.opt;
+    }
+
+    return new EventTraceController(output, traceopts);
 }
 
 } // namespace [anonymous]
