b0797303ec4cb4c071696b036dd0a7ed51302ed1
David Boehme <boehme3@llnl.gov>
2020-04-02 14:23:41 -0700

    Update travis config

---
+++ cmake/hostconfig/travis.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "")
+
+set(WITH_ADIAK        Off CACHE BOOL "")
+set(WITH_CALLPATH     On  CACHE BOOL "")
+set(WITH_NVPROF       Off CACHE BOOL "")
+set(WITH_CUPTI        Off CACHE BOOL "")
+set(WITH_PAPI         Off CACHE BOOL "")
+set(WITH_LIBPFM       Off CACHE BOOL "")
+set(WITH_DYNINST      Off CACHE BOOL "")
+set(WITH_SAMPLER      On  CACHE BOOL "")
+set(WITH_MPIT         Off CACHE BOOL "")
+set(WITH_VTUNE        Off CACHE BOOL "")
+set(WITH_SYMBOLLOOKUP On  CACHE BOOL "")
+
+set(WITH_DOCS         Off CACHE BOOL "")
+set(BUILD_TESTING     On  CACHE BOOL "")
