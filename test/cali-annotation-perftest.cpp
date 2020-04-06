c3cb1ccce5539f43a374d07fe9b7349001960c61
David Boehme <boehme3@llnl.gov>
2020-04-06 12:19:58 -0700

    Re-enable spot controller in annotation perftest

--- test/cali-annotation-perftest.cpp
+++ test/cali-annotation-perftest.cpp
@@ -30,6 +30,13 @@
 #include <caliper/cali.h>
 #include <caliper/tools-util/Args.h>
 
+#ifdef CALIPER_HAVE_MPI
+extern "C"
+{
+void cali_mpi_init(); // init spot controller
+}
+#endif
+
 #include <chrono>
 #include <iostream>
 
@@ -132,6 +139,7 @@ void record_globals(const Config& cfg, int threads, const cali::ConfigManager::a
 
 int main(int argc, char* argv[])
 {
+    cali_mpi_init(); // init spot controller
     cali_config_preset("CALI_ATTRIBUTE_DEFAULT_SCOPE", "process");
 
     const util::Args::Table option_table[] = {
