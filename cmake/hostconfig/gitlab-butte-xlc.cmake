546a6b82914a8c953c7a83883f80dfb7ab5a86df
David Boehme <boehme3@llnl.gov>
2020-07-22 14:21:50 -0700

    Update gitlab cmake configs

--- cmake/hostconfig/gitlab-butte-xlc.cmake
+++ cmake/hostconfig/gitlab-butte-xlc.cmake
@@ -1,16 +1,16 @@
-set(CMAKE_C_COMPILER   "/usr/tce/packages/xl/xl-2019.02.07/bin/xlc" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/xl/xl-2019.02.07/bin/xlC" CACHE PATH "")
-set(CMAKE_Fortran_COMPILER "/usr/tce/packages/xl/xl-2019.02.07/bin/xlf" CACHE PATH "")
+set(CMAKE_C_COMPILER   "/usr/tce/packages/xl/xl-2020.06.25/bin/xlc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/xl/xl-2020.06.25/bin/xlC" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/xl/xl-2020.06.25/bin/xlf" CACHE PATH "")
 
 # set(MPI_C_COMPILER     "/usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-xl-2019.02.07/bin/mpicc" CACHE PATH "")
 # set(MPI_CXX_COMPILER   "/usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-xl-2019.02.07/bin/mpicxx" CACHE PATH "")
 
-set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-9.2.148" CACHE PATH "")
-set(CUPTI_PREFIX "/usr/tce/packages/cuda/cuda-9.2.148/extras/CUPTI" CACHE PATH "")
+set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-10.1.243" CACHE PATH "")
+set(CUPTI_PREFIX "/usr/tce/packages/cuda/cuda-10.1.243/extras/CUPTI" CACHE PATH "")
 
 # Our f2003 requires some extra options for xlf. Skip it for now. 
 set(WITH_FORTRAN  Off CACHE BOOL "")
-set(WITH_CALLPATH Off CACHE BOOL "")
+set(WITH_CALLPATH On  CACHE BOOL "")
 set(WITH_NVPROF   On  CACHE BOOL "")
 set(WITH_CUPTI    On  CACHE BOOL "")
 set(WITH_PAPI     Off CACHE BOOL "")
@@ -18,7 +18,6 @@ set(WITH_LIBPFM   Off CACHE BOOL "")
 set(WITH_DYNINST  Off CACHE BOOL "") # turn off for now
 set(WITH_SAMPLER  On  CACHE BOOL "")
 set(WITH_MPI      On  CACHE BOOL "")
-set(WITH_MPIT     Off CACHE BOOL "") # default toss3 mvapich2-2.2 doesn't have any MPI-T stuff
 set(WITH_GOTCHA   On  CACHE BOOL "")
 set(WITH_VTUNE    Off CACHE BOOL "")
 set(WITH_SYMBOLLOOKUP On CACHE BOOL "")
--- cmake/hostconfig/gitlab-quartz-pgi.cmake
+++ cmake/hostconfig/gitlab-quartz-pgi.cmake
@@ -28,6 +28,7 @@ set(WITH_MPIT     Off CACHE BOOL "") # default toss3 mvapich2-2.2 doesn't have a
 set(WITH_GOTCHA   On  CACHE BOOL "")
 set(WITH_VTUNE    On  CACHE BOOL "")
 set(WITH_SYMBOLLOOKUP On CACHE BOOL "")
+set(WITH_PCP      On  CACHE BOOL "")
 
 set(WITH_DOCS     Off CACHE BOOL "")
 set(BUILD_TESTING On  CACHE BOOL "")
