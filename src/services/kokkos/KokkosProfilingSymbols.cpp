fe7917609915c7eafeba24c20fb7b417a2f45494
David Poliakoff <david.poliakoff@gmail.com>
2020-08-07 12:19:03 -0700

    Added fence events to the interface

--- src/services/kokkos/KokkosProfilingSymbols.cpp
+++ src/services/kokkos/KokkosProfilingSymbols.cpp
@@ -28,6 +28,9 @@ extern "C" void kokkosp_begin_parallel_reduce(const char* name, const uint32_t d
 extern "C" void kokkosp_begin_parallel_scan(const char* name, const uint32_t devID, uint64_t* kID) {
     kokkosp_callbacks.kokkosp_begin_parallel_scan_callback(name, devID, kID);
 }
+extern "C" void kokkosp_begin_fence(const char* name, const uint32_t devID, uint64_t* kID) {
+    kokkosp_callbacks.kokkosp_begin_fence_callback(name, devID, kID);
+}
 
 extern "C" void kokkosp_end_parallel_for(const uint64_t kID) {
     kokkosp_callbacks.kokkosp_end_parallel_for_callback(kID);
@@ -37,6 +40,9 @@ extern "C" void kokkosp_end_parallel_reduce(const uint64_t kID) {
 }
 extern "C" void kokkosp_end_parallel_scan(const uint64_t kID) {
     kokkosp_callbacks.kokkosp_end_parallel_scan_callback(kID);
+}
+extern "C" void kokkosp_end_fence(const uint64_t kID) {
+    kokkosp_callbacks.kokkosp_end_fence_callback(kID);
 }
 
 extern "C" void kokkosp_push_profile_region(char* regionName) {
