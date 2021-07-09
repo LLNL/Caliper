78473468abd97015d379a68974c329d31288bec3
David Boehme <boehme3@llnl.gov>
2021-07-08 17:35:05 -0700

    Update github actions script

---
+++ cmake/hostconfig/github-actions.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "")
+
+set(WITH_ADIAK        Off CACHE BOOL "")
+set(WITH_CUPTI        Off CACHE BOOL "")
+set(WITH_FORTRAN      On  CACHE BOOL "")
+set(WITH_LIBDW        On  CACHE BOOL "")
+set(WITH_LIBPFM       Off CACHE BOOL "")
+set(WITH_LIBUNWIND    On  CACHE BOOL "")
+set(WITH_MPI          Off CACHE BOOL "")
+set(WITH_MPIT         Off CACHE BOOL "")
+set(WITH_NVPROF       Off CACHE BOOL "")
+set(WITH_PAPI         Off CACHE BOOL "")
+set(WITH_SAMPLER      On  CACHE BOOL "")
+set(WITH_VTUNE        Off CACHE BOOL "")
+
+set(WITH_DOCS         Off CACHE BOOL "")
+set(BUILD_TESTING     On  CACHE BOOL "")
