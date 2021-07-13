ef68cadf9dd73a3d6c357aa8c1b28826afb6b24f
David Boehme <boehme3@llnl.gov>
2021-07-13 16:50:23 -0700

    Documentation updates

--- python/caliper-reader/caliperreader/caliperstreamreader.py
+++ python/caliper-reader/caliperreader/caliperstreamreader.py
@@ -50,7 +50,7 @@ class CaliperStreamReader:
 
 
     def attributes(self):
-        """ Return the attribute keys.
+        """ Return the list of attribute names.
 
         A file must have been read with read().
 
