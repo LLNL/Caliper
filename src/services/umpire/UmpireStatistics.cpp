bd7edb6c7db80df58d88683a57c2882f1c22a6e2
David Boehme <boehme3@llnl.gov>
2022-01-28 11:55:40 -0800

    umpire: Add and per_allocator_statistics option

--- src/services/umpire/UmpireStatistics.cpp
+++ src/services/umpire/UmpireStatistics.cpp
@@ -27,46 +27,49 @@ namespace
 
 class UmpireService
 {
+    static const ConfigSet::Entry s_configdata[];
+
     Attribute m_alloc_name_attr;
     Attribute m_alloc_current_size_attr;
     Attribute m_alloc_actual_size_attr;
     Attribute m_alloc_hwm_attr;
+    Attribute m_alloc_count_attr;
     Attribute m_total_size_attr;
+    Attribute m_total_count_attr;
 
     Node      m_root_node;
+
+    bool      m_per_allocator_stats;
 
     void process_allocator(Caliper* c, Channel* channel, const std::string& name, umpire::Allocator& alloc, SnapshotView context) {
         uint64_t actual_size = alloc.getActualSize();
         uint64_t current_size = alloc.getCurrentSize();
         uint64_t hwm = alloc.getHighWatermark();
+        uint64_t count = alloc.getAllocationCount();
 
         Attribute attr[] = {
             m_alloc_name_attr,
             m_alloc_actual_size_attr,
             m_alloc_current_size_attr,
-            m_alloc_hwm_attr
+            m_alloc_hwm_attr,
+            m_alloc_count_attr
         };
         Variant   data[] = {
             Variant(name.c_str()),
             Variant(actual_size),
             Variant(current_size),
-            Variant(hwm)
+            Variant(hwm),
+            Variant(count)
         };
 
         FixedSizeSnapshotRecord<64> rec;
         rec.builder().append(context);
 
-        c->make_record(4, attr, data, rec.builder(), &m_root_node);
+        c->make_record(5, attr, data, rec.builder(), &m_root_node);
         channel->events().process_snapshot(c, channel, SnapshotView(), rec.view());
     }
 
     void snapshot(Caliper* c, Channel* channel, SnapshotView info, SnapshotBuilder& snapshot_rec) {
-        auto& rm = umpire::ResourceManager::getInstance();
-        auto allocators = rm.getAllocatorNames();
-
-        if (allocators.empty())
-            return;
-
         //   Bit of a hack: We create one record for each allocator for
         // allocator-specific info. This way we can use generic allocator.name
         // and allocator.size attributes. To avoid issues with repeated
@@ -74,18 +77,29 @@ class UmpireService
         // context info and move the records directly to postprocessing.
 
         FixedSizeSnapshotRecord<60> context;
-        context.builder().append(info);
-        c->pull_context(channel, CALI_SCOPE_PROCESS | CALI_SCOPE_THREAD, context.builder());
 
-        uint64_t total_size = 0;
-
-        for (auto s : allocators) {
-            auto alloc = rm.getAllocator(s);
-            total_size += alloc.getCurrentSize();
-            process_allocator(c, channel, s, alloc, context.view());
+        if (m_per_allocator_stats) {
+            context.builder().append(info);
+            c->pull_context(channel, CALI_SCOPE_PROCESS | CALI_SCOPE_THREAD, context.builder());
         }
 
-        snapshot_rec.append(m_total_size_attr, Variant(total_size));
+        uint64_t total_size  = 0;
+        uint64_t total_count = 0;
+
+        auto& rm = umpire::ResourceManager::getInstance();
+
+        for (auto s : rm.getAllocatorNames()) {
+            auto alloc = rm.getAllocator(s);
+
+            total_size  += alloc.getCurrentSize();
+            total_count += alloc.getAllocationCount();
+
+            if (m_per_allocator_stats)
+                process_allocator(c, channel, s, alloc, context.view());
+        }
+
+        snapshot_rec.append(m_total_size_attr,  Variant(total_size));
+        snapshot_rec.append(m_total_count_attr, Variant(total_count));
     }
 
     void finish_cb(Caliper* c, Channel* channel) {
@@ -114,16 +128,32 @@ class UmpireService
                                 CALI_TYPE_UINT,
                                 CALI_ATTR_ASVALUE | CALI_ATTR_SKIP_EVENTS,
                                 1, &class_aggregatable_attr, &v_true);
+        m_alloc_count_attr =
+            c->create_attribute("umpire.alloc.count",
+                                CALI_TYPE_UINT,
+                                CALI_ATTR_ASVALUE | CALI_ATTR_SKIP_EVENTS,
+                                1, &class_aggregatable_attr, &v_true);
         m_total_size_attr =
             c->create_attribute("umpire.total.size",
+                                CALI_TYPE_UINT,
+                                CALI_ATTR_ASVALUE | CALI_ATTR_SKIP_EVENTS,
+                                1, &class_aggregatable_attr, &v_true);
+        m_total_count_attr =
+            c->create_attribute("umpire.total.count",
                                 CALI_TYPE_UINT,
                                 CALI_ATTR_ASVALUE | CALI_ATTR_SKIP_EVENTS,
                                 1, &class_aggregatable_attr, &v_true);
     }
 
     UmpireService(Caliper* c, Channel* channel)
-        : m_root_node(CALI_INV_ID, CALI_INV_ID, Variant())
+        : m_root_node(CALI_INV_ID, CALI_INV_ID, Variant()),
+          m_per_allocator_stats(true)
         {
+            auto config = channel->config().init("umpire", s_configdata);
+
+            m_per_allocator_stats =
+                config.get("per_allocator_statistics").to_bool();
+
             create_attributes(c);
         }
 
@@ -145,6 +175,14 @@ public:
         Log(1).stream() << channel->name() << ": Registered umpire service"
                         << std::endl;
     }
+};
+
+const ConfigSet::Entry UmpireService::s_configdata[] = {
+    { "per_allocator_statistics", CALI_TYPE_BOOL, "true",
+      "Include statistics for each Umpire allocator",
+      "Include statistics for each Umpire allocator"
+    },
+    ConfigSet::Terminator
 };
 
 } // namespace [anonymous]
