36139ee5fbf02dcc44b00950810dd08e583dd448
David Boehme <boehme3@llnl.gov>
2023-07-26 09:59:43 -0700

    Update internal gotcha to v1.0.4 (#498)

--- ext/gotcha/src/gotcha_dl.h
+++ ext/gotcha/src/gotcha_dl.h
@@ -5,7 +5,7 @@
 #include "tool.h"
 
 void handle_libdl();
-extern void update_all_library_gots(hash_table_t *bindings);
+extern void update_all_library_gots(hash_table_t *bindings, int lookup_rel);
 extern long lookup_exported_symbol(const char* name, const struct link_map *lib, void** symbol);
 extern int prepare_symbol(struct internal_binding_t *binding);
 
