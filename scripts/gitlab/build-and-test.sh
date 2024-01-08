8fea00f65a9acbc6cc4f93d8b974422193e729cd
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2024-01-08 14:07:44 +0100

    Update build and test script with other projects

--- scripts/gitlab/build-and-test.sh
+++ scripts/gitlab/build-and-test.sh
@@ -1,19 +1,47 @@
 #!/usr/bin/env bash
 
-hostname="$(hostname)"
-timestamp="$(date +"%T" | sed 's/://g')"
-project_dir="$(pwd)"
-hostname="$(hostname)"
-spec=${SPEC:-""}
+# Initialize modules for users not using bash as a default shell
+if test -e /usr/share/lmod/lmod/init/bash
+then
+  . /usr/share/lmod/lmod/init/bash
+fi
+
+###############################################################################
+# Copyright (c) 2016-23, Lawrence Livermore National Security, LLC and Caliper
+# project contributors. See the Caliper/LICENSE file for details.
+#
+# SPDX-License-Identifier: (BSD-3-Clause)
+###############################################################################
+
+set -o errexit
+set -o nounset
+
 option=${1:-""}
+hostname="$(hostname)"
 truehostname=${hostname//[0-9]/}
+project_dir="$(pwd)"
+
+hostconfig=${HOST_CONFIG:-""}
+spec=${SPEC:-""}
+module_list=${MODULE_LIST:-""}
+job_unique_id=${CI_JOB_ID:-""}
+use_dev_shm=${USE_DEV_SHM:-true}
+
+if [[ -n ${module_list} ]]
+then
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~~~ Modules to load: ${module_list}"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    module load ${module_list}
+fi
 
 prefix=""
 
 if [[ "${option}" == "--project-build" ]]
 then
+    timestamp="$(date +"%T" | sed 's/://g')"
     prefix=${project_dir}"/CI-builds/${hostname}-${timestamp}"
-elif [[ -d /dev/shm ]]
+elif [[ -d /dev/shm && ${use_dev_shm} == true ]]
 then
     prefix="/dev/shm/${hostname}"
     if [[ -z ${job_unique_id} ]]; then
@@ -23,7 +51,11 @@ then
           job_unique_id=manual_job_$(date +%s)
       done
     fi
+
     prefix="${prefix}-${job_unique_id}"
+else
+    # We set the prefix in the parent directory so that spack dependencies are not installed inside the source tree.
+    prefix="$(pwd)/../spack-and-build-root"
 fi
 
 echo "Creating directory ${prefix}"
@@ -31,26 +63,42 @@ echo "project_dir: ${project_dir}"
 
 mkdir -p ${prefix}
 
-spack_user_cache="${prefix}/spack-user-cache"
-export SPACK_DISABLE_LOCAL_CONFIG=""
-export SPACK_USER_CACHE_PATH="${spack_user_cache}"
-mkdir -p ${spack_user_cache}
+# Dependencies
+if [[ "${option}" != "--build-only" && "${option}" != "--test-only" ]]
+then
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~~~ Building dependencies $(date)"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-if [[ -z ${spec} ]]
+    if [[ -z ${spec} ]]
     then
         echo "SPEC is undefined, aborting..."
         exit 1
     fi
 
-# generate cmake cache file with uberenv and radiuss spack package
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "~~~~~ Building Dependencies $(date)"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-./scripts/uberenv/uberenv.py --prefix ${prefix} --spec="${spec}"
+    prefix_opt="--prefix=${prefix}"
 
-# find generated cmake cache
+    # We force Spack to put all generated files (cache and configuration of
+    # all sorts) in a unique location so that there can be no collision
+    # with existing or concurrent Spack.
+    spack_user_cache="${prefix}/spack-user-cache"
+    export SPACK_DISABLE_LOCAL_CONFIG=""
+    export SPACK_USER_CACHE_PATH="${spack_user_cache}"
+    mkdir -p ${spack_user_cache}
+
+    # generate cmake cache file with uberenv and radiuss spack package
+    ./scripts/uberenv/uberenv.py --spec="${spec}" ${prefix_opt}
+
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~~~ Dependencies built $(date)"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+fi
+
+# find cmake cache file (hostconfig)
 if [[ -z ${hostconfig} ]]
 then
+    # If no host config file was provided, we assume it was generated.
+    # This means we are looking of a unique one in project dir.
     hostconfigs=( $( ls "${project_dir}/"*.cmake ) )
     if [[ ${#hostconfigs[@]} == 1 ]]
     then
@@ -68,17 +116,21 @@ then
         exit 1
     fi
 else
-    hostconfig_path = "${project_dir}/${hostconfig}"
+    # Using provided host-config file.
+    hostconfig_path="${project_dir}/${hostconfig}"
 fi
 
 hostconfig=$(basename ${hostconfig_path})
 echo "Found hostconfig: ${hostconfig}"
 
-# build directory
-build_dir="${prefix}/build_${hostconfig//.cmake/}"
-install_dir="${prefix}/install_${hostconfig//.cmake/}"
-cmake_exe=`grep 'CMake executable' ${hostconfig_path} | cut -d ':' -f 2 | xargs`
+# Build Directory
+# When using /dev/shm, we use prefix for both spack builds and source build, unless BUILD_ROOT was defined
+build_root=${BUILD_ROOT:-"${prefix}"}
 
+build_dir="${build_root}/build_${hostconfig//.cmake/}"
+install_dir="${build_root}/install_${hostconfig//.cmake/}"
+
+cmake_exe=`grep 'CMake executable' ${hostconfig_path} | cut -d ':' -f 2 | xargs`
 
 # Build
 if [[ "${option}" != "--deps-only" && "${option}" != "--test-only" ]]
@@ -105,6 +157,7 @@ then
     rm -rf ${build_dir} 2>/dev/null
     mkdir -p ${build_dir} && cd ${build_dir}
 
+    date
     if [[ "${truehostname}" == "corona" || "${truehostname}" == "tioga" ]]
     then
         module unload rocm
@@ -124,10 +177,13 @@ then
     fi
     if ! $cmake_exe --build . -j ${core_counts[$truehostname]}
     then
-        echo "ERROR: compilation failed, building with verbose output..."
+        echo "[Error]: compilation failed, building with verbose output..."
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        echo "~~~~~ Running make VERBOSE=1 $(date)"
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         $cmake_exe --build . --verbose -j 1
     else
-        make install
+        $cmake_exe --install .
     fi
 
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -142,18 +198,21 @@ then
     echo "~~~~~ Testing Caliper $(date)"
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-    echo "Moving to ${build_dir}"
+    if [[ ! -d ${build_dir} ]]
+    then
+        echo "[Error]: Build directory not found : ${build_dir}" && exit 1
+    fi
+
     cd ${build_dir}
 
     date
     ctest --output-on-failure -T test 2>&1 | tee tests_output.txt
     date
 
-
     no_test_str="No tests were found!!!"
     if [[ "$(tail -n 1 tests_output.txt)" == "${no_test_str}" ]]
     then
-        echo "ERROR: No tests were found" && exit 1
+        echo "[Error]: No tests were found" && exit 1
     fi
 
     echo "Copying Testing xml reports for export"
@@ -163,7 +222,7 @@ then
 
     if grep -q "Errors while running CTest" ./tests_output.txt
     then
-        echo "ERROR: failure(s) while running CTest" && exit 1
+        echo "[Error]: failure(s) while running CTest" && exit 1
     fi
 
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
