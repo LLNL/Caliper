f4ccc82f5f7edbb6b48ae67437c2413cb193378c
Tapasya Patki <patki1@llnl.gov>
2024-08-13 15:05:25 -0700

    Add a new spinloop based example with function begin/end markers for testing.

---
+++ examples/apps/c-spinloop-example.c
@@ -0,0 +1,48 @@
+// Copyright (c) 2015-2022, Lawrence Livermore National Security, LLC.
+// See top-level LICENSE file for details.
+
+// A C Caliper instrumentation and ConfigManager example
+
+//   Usage: $ cali-basic-annotations <configuration-string>
+// For example, "$ cali-basic-annotations runtime-report" will print a
+// hierarchical runtime summary for all annotated regions.
+
+#include <caliper/cali.h>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define LARGE_NUM 100000000
+
+void bar()
+{
+    printf("Enter Bar. Sleep for a second.\n");
+    CALI_MARK_FUNCTION_BEGIN;
+    sleep(1);
+    CALI_MARK_FUNCTION_END;
+    printf("Exit Bar. \n");
+}
+
+void foo()
+{
+    printf("Enter Foo. Run a long spinloop\n");
+    CALI_MARK_FUNCTION_BEGIN;
+    long double res=0.1;
+    int i;
+    for (i=0;i<100000000;i++)
+        res += res * i;
+    CALI_MARK_FUNCTION_END;
+    printf("Exit Foo. \n");
+}
+
+int main(int argc, char* argv[])
+{
+    // Mark begin of the current function. Must be manually closed.
+    CALI_MARK_FUNCTION_BEGIN;
+    printf("Hello World\n");
+    foo();
+    bar();
+    // Mark the end of the current function
+    CALI_MARK_FUNCTION_END;
+}
