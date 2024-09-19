40293424a601d0924a5c63b7f0c3a1cfb165e1e7
Ian Lumsden <lumsden.ian@gmail.com>
2024-09-19 18:13:31 -0400

    Adds Python bindings to Caliper with pybind11 (#573)
    * Implements Python bindings for Caliper
    
    * Improves the exported type enums
    
    * Adds examples of the Python API
    
    * Fixes default parameter for cali_attr_properties
    
    * Removes default arugments for Attribute and Annotation in place of multiple constructors
    
    * Adds unit tests for Python bindings
    
    * Adds docstrings to Python bindings
    
    * Moves Python bindings to src/interface and adds logic to help with unit testing
    
    * Updates GH Actions runner to install Pybind11 correctly
    
    * Removes Variant from Python bindings
    
    * Removes redundant install of Pybind11 with apt
    
    * Enables bitwise arithmetic in bindings for cali_attr_properties

---
+++ src/interface/python/loop.h
@@ -0,0 +1,26 @@
+#ifndef CALI_INTERFACE_PYTHON_LOOP_H
+#define CALI_INTERFACE_PYTHON_LOOP_H
+
+#include "common.h"
+
+namespace cali {
+
+class PythonLoop {
+public:
+  PythonLoop(const char *name);
+
+  void start_iteration(int i);
+
+  void end_iteration();
+
+  void end();
+
+private:
+  cali_id_t m_iter_attr;
+};
+
+void create_caliper_loop_mod(py::module_ &caliper_loop_mod);
+
+} // namespace cali
+
+#endif /* CALI_INTERFACE_PYTHON_LOOP_H */
---
+++ src/interface/python/pycaliper/__init__.py
@@ -0,0 +1,15 @@
+from pycaliper.__pycaliper_impl import (
+    __version__,
+    config_preset,
+    init,
+    is_initialized,
+)
+
+import pycaliper.annotation
+import pycaliper.config_manager
+import pycaliper.high_level
+import pycaliper.instrumentation
+import pycaliper.loop
+import pycaliper.types
+
+from pycaliper.high_level import annotate_function
