ba828a9975f34c6f9065cf29de5f882fdec52283
Ian Lumsden <lumsden.ian@gmail.com>
2024-10-10 13:15:34 -0400

    Adds scripts to do autoformatting with Docker to ensure the right version of clang-format is used

--- scripts/formatting/autoformat.sh
+++ scripts/formatting/autoformat.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#! /bin/bash
 
-SUPPORTED_CLANG_FORMAT_VERSION="16.0.0"
+clang_format_exe="clang-format"
+if [ $# -ge 1 ]; then
+    clang_format_exe="$1"
+fi
 
-if [ command -v clang-format >/dev/null 2>&1 ]; then
+SUPPORTED_CLANG_FORMAT_VERSION="16.0.6"
+
+if [ command -v $clang_format_exe >/dev/null 2>&1 ]; then
     echo "You must have 'clang-format' in PATH to use 'autoformat.sh'"
     exit 1
 fi
 
-clang_format_version_str=$(clang-format --version)
+clang_format_version_str=$($clang_format_exe --version)
 clang_format_version=$(echo "$clang_format_version_str" | grep -oP 'clang-format version \K\d+(\.\d+)+')
 
 if [ "$clang_format_version" != "$SUPPORTED_CLANG_FORMAT_VERSION" ]; then
@@ -37,10 +42,18 @@ curr_dir=$(pwd)
 
 cd $SCRIPT_DIR
 cd ..
+cd ..
 
-find src -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs clang-format -i
-find include -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs clang-format -i
-find examples -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs clang-format -i
-find test -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs clang-format -i
+echo "Formatting C/C++ code in 'src'"
+find src -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs $clang_format_exe -i
+
+echo "Formatting C/C++ code in 'include'"
+find include -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs $clang_format_exe -i
+
+echo "Formatting C/C++ code in 'examples'"
+find examples -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs $clang_format_exe -i
+
+echo "Formatting C/C++ code in 'test'"
+find test -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" | xargs $clang_format_exe -i
 
 cd $curr_dir
